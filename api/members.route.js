46e80a8c6e2ba43dc396232896ca45eb42fb2f4d
Pratush Agrawal <67389873+par26@users.noreply.github.com>
2023-09-16 16:33:43 -0700

    Update members.route.js

--- api/members.route.js
+++ api/members.route.js
@@ -4,5 +4,6 @@ import MembersCtrl from "./members.controller.js"
 const router = express.Router()
 
 router.route("/new").post(MembersCtrl.apiAddMembers)
+router.route("/new").get((req, res) => res.status(500).json({ error: "Poggers I think we're getting somewhere" }))
 
 export default router
