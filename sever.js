1c4f1fed349929b0cd65a3ac73e60c650475eaa3
Pratush Agrawal <67389873+par26@users.noreply.github.com>
2023-09-16 14:56:35 -0700

    Update sever.js

--- sever.js
+++ sever.js
@@ -1,17 +1,22 @@
-import express from "express"
-//import cors from "cors";
-import members from "/api/members.route.js"
+import express from "express";
+import members from "./api/members.route.js"; // Corrected path
 
+const app = express();
 
-const app = express()
+// CORS setup
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*"); // Replace * with your allowed origins.
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 
+app.use(express.json());
 
-//app.use(cors())
-app.use(express.json())
+// Mount the members router at the specified path
+app.use("/api/v1/members", members); // Added a slash (/) before "api/v1/members"
 
+// Error handling middleware (handle 404 errors)
+app.use("*", (req, res) => res.status(404).json({ error: "Not valid request/url" }));
 
-app.use("api/v1/members", members)
-app.use("*", (res, req) => res.status(404).json({error: "Not valid request/url"}))
-
-
-export default app
+export default app;
